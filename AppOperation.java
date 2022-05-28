52a6a02c8ad13d07021a6135e9e83c236f02a707
Eliab11 <102763493+Eliab11@users.noreply.github.com>
2022-05-27 23:43:36 -0700

    fil#1

---
+++ AppOperation.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class AppOperation {
+	Scanner scnr = new Scanner(System.in);
+	public static void main(String[] args) {
+    System.out.println("======================================================");
+	System.out.println("==                  WELCOME TO LOCKEDME.COM                                        ==");
+	System.out.println("==           DEVELOPPED BY ESSEH  ALAGAH KOMLAVI                           ==");
+	System.out.println("======================================================");
+	System.out.println();
+	System.out.println();
+	
+	System.out.println("======================================================");
+	System.out.println("==                  THE APPLICATION  IS DISIGNED TO:                                ==");
+	System.out.println("==                    TO ADD FILE TO A DIRECTORY                                       ==");
+	System.out.println("==                     TO DELETE FILE  IN A DIRECTORYI                                ==");
+	System.out.println("==                    TO SEARCH  FILE IN  A DIRECTORY                                ==");
+	System.out.println("==                    TO RETRIEVE  FILE IN  ASCENDING ORDER                   ==");
+	System.out.println("======================================================");
+	System.out.println();
+	System.out.println();
+	System.out.println();
+
+	 MnOperation1.MnOP();
+	}
+
+}
